b5cf031c67b3f93bfefed5c00af74a5f27131384
Matthies Abera <s3779748@student.rmit.edu.au>
2020-04-08 12:34:20 +1000

    Touched up CSS to show multiple results neatly, showActor complete now

--- src/main/java/app/controller/ShowController.java
+++ src/main/java/app/controller/ShowController.java
@@ -1,8 +1,8 @@
 package app.controller;
-
 import app.controller.paths.Template;
 import app.controller.utils.ViewUtil;
 import app.dao.ShowDAO;
+import app.model.CreditsRoll;
 import app.model.Show;
 import io.javalin.http.Handler;
 
@@ -10,26 +10,31 @@ import java.util.List;
 import java.util.Map;
 
 
-
-public class ShowController {
-
-    public static Handler serveShowPage = ctx -> {
+public class ShowController
+{
+    public static Handler serveShowPage = ctx ->
+    {
         Map<String, Object> model = ViewUtil.baseModel(ctx);
         // You'll have to update the model... maybe here
 
-        Show show = ShowDAO.getShowByID("1");
+        Show show = ShowDAO.getShowByID(ctx.queryParam("show"));
+        List<CreditsRoll> creditsRoll = ShowDAO.getCreditsRollByShowID(ctx.queryParam("show"));
+        String duration = Double.toString(show.getLength());
+
         model.put("show", show);
+        model.put("hour",duration.substring(0,1));
+        model.put("minutes", duration.substring(2));
+        model.put("credits", creditsRoll);
         ctx.render(Template.SHOW, model);
-        };
+    };
 
-        public static Handler serveSearchByActorPage = ctx -> {
-            Map<String, Object> model = ViewUtil.baseModel(ctx);
-
-            String searchText = ctx.queryParam("showActorSearch");
-            List<Show> shows = ShowDAO.getAllShowsByPersonFilter(searchText);
-            model.put("shows", shows);
-            model.put("query", searchText);
-            ctx.render(Template.SEARCH_BY_ACTOR, model);
-    };  
-
+    public static Handler serveSearchByActorPage = ctx ->
+    {
+        Map<String, Object> model = ViewUtil.baseModel(ctx);
+        String searchText = ctx.queryParam("showActorSearch");
+        List<Show> shows = ShowDAO.getAllShowsByPersonFilter(searchText);
+        model.put("shows", shows);
+        model.put("query", searchText);
+        ctx.render(Template.SEARCH_BY_ACTOR, model);
+    };
 }
