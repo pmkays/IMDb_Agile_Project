c5ce607ab452688322579f7bfca1368ad14ec4b8
Josiah Miranda <s3782051@student.rmit.edu.au>
2020-04-27 23:45:48 +1000

    Created Account Page and implemented relevant functionality

--- src/main/java/app/dao/UserReviewDAO.java
+++ src/main/java/app/dao/UserReviewDAO.java
@@ -133,6 +133,48 @@ public class UserReviewDAO {
         // If we are here, something bad happened
         return null;
     }
+    
+    public static List<UserReview> getReviewsByUser(String userID) {
+        // Fish out the results
+        List<UserReview> userReviews = new ArrayList<>();
+
+        try {
+            // Here you prepare your sql statement
+            String sql = "SELECT * FROM user_review WHERE user_id='" + userID + "';";
+
+            // Execute the query
+            Connection connection = DatabaseUtils.connectToDatabase();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            // If you have multiple results, you do a while
+            while(result.next()) {
+                // 2) Add it to the list we have prepared
+            	int reviewID = result.getInt("reviewId");
+        		int retrievedShowID = result.getInt("show_id");
+        		String retrievedUserID = result.getString("user_id");
+        		int rating = result.getInt("rating");
+        		String title = result.getString("title");
+        		String review = result.getString("review");
+        		Date date = result.getDate("date");
+        		
+                userReviews.add(
+                  new UserReview(reviewID, retrievedShowID, retrievedUserID, rating, title, review, date)
+                );
+            }
+
+            DatabaseUtils.closeConnection(connection);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if(!userReviews.isEmpty()) {
+        	return userReviews;
+        }
+        // If we are here, something bad happened
+        return null;
+    }
 
 	public static void updateReview(String showID, String userID, int rating, String title, String review) {
 	       try {
