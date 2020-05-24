100821dc08069948ea7ffcb0a8c40828526b9427
Josiah Miranda <s3782051@student.rmit.edu.au>
2020-05-24 19:23:38 +1000

    Did unit testing

--- src/main/java/app/dao/AccountDAO.java
+++ src/main/java/app/dao/AccountDAO.java
@@ -10,165 +10,154 @@ import app.dao.utils.DatabaseUtils;
 import app.model.Account;
 import app.model.Show;
 
-
-
 public class AccountDAO {
-    public static final String SALT = "$2a$10$h.dl5J86rGH7I8bD9bZeZe";
+	public static final String SALT = "$2a$10$h.dl5J86rGH7I8bD9bZeZe";
 
+	/**
+	 * Method to fetch users from the database. You should use this as an example
+	 * for future queries, though the sql statement will change -and you are
+	 * supposed to write them.
+	 *
+	 * Current user: caramel 6, password (the password is "password" without quotes)
+	 * 
+	 * @param username what the user typed in the log in form.
+	 * @return Some of the user data to check on the password. Null if there no
+	 *         matching user.
+	 */
+	public static Account getUserByUsername(String username) {
+		// Fish out the results
+		List<Account> accounts = new ArrayList<>();
 
-    /**
-     * Method to fetch users from the database.
-     * You should use this as an example for future queries, though the sql statement
-     * will change -and you are supposed to write them.
-     *
-     * Current user: caramel 6, password (the password is "password" without quotes)
-     * @param username what the user typed in the log in form.
-     * @return Some of the user data to check on the password. Null if there
-     *         no matching user.
-     */
-    public static Account getUserByUsername(String username) {
-        // Fish out the results
-        List<Account> accounts = new ArrayList<>();
+		try {
+			// Here you prepare your sql statement
+			String sql = "SELECT * FROM account WHERE username ='" + username + "'";
 
-        try {
-            // Here you prepare your sql statement
-            String sql = "SELECT * FROM account WHERE username ='" + username + "'";
+			// Execute the query
+			Connection connection = DatabaseUtils.connectToDatabase();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
 
-            // Execute the query
-            Connection connection = DatabaseUtils.connectToDatabase();
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
+			// If you have multiple results, you do a while
+			while (result.next()) {
+				// 2) Add it to the list we have prepared
+				accounts.add(
+						// 1) Create a new account object
+						new Account(result.getString("username"), result.getString("password"),
+								result.getString("first_name"), result.getString("last_name"),
+								result.getString("country"), result.getString("gender"), result.getString("email"),
+								result.getString("role"), result.getString("post_code"), result.getString("year"),
+								result.getString("organisation_name"), result.getString("organisation_number"),
+								result.getInt("status"), result.getString("proco_id"))
 
-            // If you have multiple results, you do a while
-            while(result.next()) {
-                // 2) Add it to the list we have prepared
-            	accounts.add(
-                        // 1) Create a new account object
-                      		new Account(
-                      				result.getString("username"),
-                      				result.getString("password"),
-                      				result.getString("first_name"),
-                      				result.getString("last_name"),
-                      				result.getString("country"),
-                      				result.getString("gender"),
-                      				result.getString("email"),
-                      				result.getString("role"),
-                      				result.getString("post_code"),
-                      				result.getString("year"),
-                      				result.getString("organisation_name"),
-                      				result.getString("organisation_number"),
-                      				result.getInt("status"),
-                      				result.getString("proco_id"))
-                      				
-                      );
-            }
+				);
+			}
 
-            // Close it
-            DatabaseUtils.closeConnection(connection);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+			// Close it
+			DatabaseUtils.closeConnection(connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		// If there is a result
+		if (!accounts.isEmpty())
+			return accounts.get(0);
+		// If we are here, something bad happened
+		return null;
+	}
 
-        // If there is a result
-        if(!accounts.isEmpty()) return accounts.get(0);
-        // If we are here, something bad happened
-        return null;
-    }
-    
-    public static boolean addNewAccount(Account acct) {
+	public static boolean addNewAccount(Account acct) {
 		boolean success = true;
 
 		try {
 			String sql;
 
-			if(acct.getProcoId() == null) {
-				sql = String.format("INSERT INTO `account` (`username`, `password`, "
-						+ "email, country, gender, first_name, last_name, role, "
-						+ "post_code, organisation_name, organisation_number, `year`, `status`) VALUES "
-						+ "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d);", 
-						acct.getUsername(), acct.getPassword(), 
-						acct.getEmail(), acct.getCountry(), acct.getGender(), 
-						acct.getFirstName(), acct.getLastName(), acct.getRole(), 
-						acct.getPostCode(), acct.getOrganisationName(), 
-						acct.getOrganisationNumber(), acct.getYear(), acct.getStatus());
+			if (acct.getProcoId() == null) {
+				sql = String.format(
+						"INSERT INTO `account` (`username`, `password`, "
+								+ "email, country, gender, first_name, last_name, role, "
+								+ "post_code, organisation_name, organisation_number, `year`, `status`) VALUES "
+								+ "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d);",
+						acct.getUsername(), acct.getPassword(), acct.getEmail(), acct.getCountry(), acct.getGender(),
+						acct.getFirstName(), acct.getLastName(), acct.getRole(), acct.getPostCode(),
+						acct.getOrganisationName(), acct.getOrganisationNumber(), acct.getYear(), acct.getStatus());
 			} else {
 				sql = String.format("INSERT INTO `account` (`username`, `password`, "
 						+ "email, country, gender, first_name, last_name, role, "
 						+ "post_code, organisation_name, organisation_number, `year`, `status`, proco_id) VALUES "
-						+ "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d, %s);",
-						acct.getUsername(), acct.getPassword(), 
-						acct.getEmail(), acct.getCountry(), acct.getGender(), 
-						acct.getFirstName(), acct.getLastName(), acct.getRole(), 
-						acct.getPostCode(), acct.getOrganisationName(), 
+						+ "('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s',%d, %s);", acct.getUsername(),
+						acct.getPassword(), acct.getEmail(), acct.getCountry(), acct.getGender(), acct.getFirstName(),
+						acct.getLastName(), acct.getRole(), acct.getPostCode(), acct.getOrganisationName(),
 						acct.getOrganisationNumber(), acct.getYear(), acct.getStatus(), acct.getProcoId());
 			}
-			
-			
+
 			Connection connection = DatabaseUtils.connectToDatabase();
 			Statement statement = connection.createStatement();
 			statement.executeUpdate(sql);
-			
-			
+
 			statement.close();
 			DatabaseUtils.closeConnection(connection);
-			
+
 		} catch (Exception e) {
 			success = false;
 			e.printStackTrace();
 		}
-		
+
 		return success;
-		
+
 	}
 
 	public static List<Account> getPendingAccounts() {
 		// Fish out the results
-        List<Account> accounts = new ArrayList<>();
+		List<Account> accounts = new ArrayList<>();
 
-        try {
-            // Here you prepare your sql statement
-            String sql = "SELECT * FROM account WHERE status = 0";
+		try {
+			// Here you prepare your sql statement
+			String sql = "SELECT * FROM account WHERE status = 0";
 
-            // Execute the query
-            Connection connection = DatabaseUtils.connectToDatabase();
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
+			// Execute the query
+			Connection connection = DatabaseUtils.connectToDatabase();
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
 
-            // If you have multiple results, you do a while
-            while(result.next()) {
-                // 2) Add it to the list we have prepared
-            	accounts.add(
-                        // 1) Create a new account object
-                      		new Account(
-                      				result.getString("username"),
-                      				result.getString("password"),
-                      				result.getString("first_name"),
-                      				result.getString("last_name"),
-                      				result.getString("country"),
-                      				result.getString("gender"),
-                      				result.getString("email"),
-                      				result.getString("role"),
-                      				result.getString("post_code"),
-                      				result.getString("year"),
-                      				result.getString("organisation_name"),
-                      				result.getString("organisation_number"),
-                      				result.getInt("status"),
-                      				result.getString("proco_id"))
-                      );
-            }
+			// If you have multiple results, you do a while
+			while (result.next()) {
+				// 2) Add it to the list we have prepared
+				accounts.add(
+						// 1) Create a new account object
+						new Account(result.getString("username"), result.getString("password"),
+								result.getString("first_name"), result.getString("last_name"),
+								result.getString("country"), result.getString("gender"), result.getString("email"),
+								result.getString("role"), result.getString("post_code"), result.getString("year"),
+								result.getString("organisation_name"), result.getString("organisation_number"),
+								result.getInt("status"), result.getString("proco_id")));
+			}
 
-            // Close it
-            DatabaseUtils.closeConnection(connection);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return accounts;
+			// Close it
+			DatabaseUtils.closeConnection(connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return accounts;
 	}
-	
-	public static void updateAccountStatus(String username, String status) {
+
+	public static boolean updateAccountStatus(String username, String status) {
+		boolean toReturn = false;
+		Account check = getUserByUsername(username);
+		if (check == null || username == null || status == null) {
+			return false;
+		}
+		
+		int statusInt;
+		try {
+			statusInt = Integer.parseInt(status);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		
+		if (statusInt < 0 || statusInt > 2) {
+			return false;
+		}
+		
 	       try {
 	            String sql = String.format("UPDATE account " + 
 	            		"SET status = " + status + 
@@ -181,11 +170,15 @@ public class AccountDAO {
 
 	            // Close it
 	            DatabaseUtils.closeConnection(connection);
+	            
+	            toReturn = true;
 	        }
 	        catch (Exception e) {
 	            e.printStackTrace();
+	            toReturn = false;
 	        }
 		
+	       return toReturn;
 	}
 
 }
--- src/test/java/dao/AccountDAOTest.java
+++ src/test/java/dao/AccountDAOTest.java
@@ -1,5 +1,6 @@
 package dao;
 
+import app.dao.AccountDAO;
 import app.dao.utils.DatabaseUtils;
 import app.model.Account;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,51 +12,86 @@ import java.sql.Statement;
 import static app.dao.AccountDAO.addNewAccount;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AccountDAOTest
-{
+class AccountDAOTest {
 
-    @BeforeEach
-    void setUp()
-    {
-        Connection connection = null;
-        String sql = "DELETE FROM account" +
-                     " WHERE username = 'cool_abed_films' OR username = 'shaunaMalwayTweep';";
-        try{
-            connection = DatabaseUtils.connectToDatabase();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
-            statement.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+	@BeforeEach
+	void setUp() {
+		Connection connection = null;
+		String sql = "DELETE FROM account" + " WHERE username = 'cool_abed_films' OR username = 'shaunaMalwayTweep';";
+		try {
+			connection = DatabaseUtils.connectToDatabase();
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			statement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        DatabaseUtils.closeConnection(connection);
-    }
+		DatabaseUtils.closeConnection(connection);
+	}
 
-    @Test
-    void addNewAccount_successTrue_validPCoAccount()
-    {
-        Account pcoAccount = new Account("cool_abed_films", "GreenDale33", "Abed",
-                "Nadir", "Poland", "Male", "coolabedfilms@gmail.com", "pco",
-                "4177", "2020", "Warner Bros.",
-                "2", 2, "4");
-        assertTrue(addNewAccount(pcoAccount));
-    }
+	@Test
+	void addNewAccount_successTrue_validPCoAccount() {
+		Account pcoAccount = new Account("cool_abed_films", "GreenDale33", "Abed", "Nadir", "Poland", "Male",
+				"coolabedfilms@gmail.com", "pco", "4177", "2020", "Warner Bros.", "2", 2, "4");
+		assertTrue(addNewAccount(pcoAccount));
+	}
 
-    @Test
-    void addNewAccount_successTrue_validCriticAccount()
-    {
-        Account criticAccount = new Account("shaunaMalwayTweep", "pTimes78", "Shauna",
-                "Malway-Tweep", "USA", "Female", "shauna@pawneejournal.com", "critic",
-                "3106", "2012", "Pawnee Journal",
-                "3", 2);
-        assertTrue(addNewAccount(criticAccount));
-    }
+	@Test
+	void addNewAccount_successTrue_validCriticAccount() {
+		Account criticAccount = new Account("shaunaMalwayTweep", "pTimes78", "Shauna", "Malway-Tweep", "USA", "Female",
+				"shauna@pawneejournal.com", "critic", "3106", "2012", "Pawnee Journal", "3", 2);
+		assertTrue(addNewAccount(criticAccount));
+	}
 
-    @Test
-    void addNewAccount_successFalse_invalidAccount()
-    {
-        Account nullAccount = null;
-        assertFalse(addNewAccount(nullAccount));
-    }
+	@Test
+	void addNewAccount_successFalse_invalidAccount() {
+		Account nullAccount = null;
+		assertFalse(addNewAccount(nullAccount));
+	}
+
+
+	@Test
+	void updateAccountStatus_successTrue_rejectAccount() {
+		boolean success = AccountDAO.updateAccountStatus("caramel6", "1");
+		assertTrue(success);
+	}
+
+	@Test
+	void updateAccountStatus_successTrue_approveAccount() {
+		boolean success = AccountDAO.updateAccountStatus("caramel6", "2");
+		assertTrue(success);
+	}
+	
+	@Test
+	void updateAccountStatus_successFalse_bothNullInput() {
+		boolean success = AccountDAO.updateAccountStatus(null, null);
+		assertFalse(success);
+	}
+	
+	@Test
+	void updateAccountStatus_successFalse_nullUsername() {
+		boolean success = AccountDAO.updateAccountStatus(null, "1");
+		assertFalse(success);
+	}
+	
+	@Test
+	void updateAccountStatus_successFalse_invalidUsername() {
+		boolean success = AccountDAO.updateAccountStatus("xX_The_Sasuke_Lover_Xx", "2");
+		assertFalse(success);
+	}
+	
+	@Test
+	void updateAccountStatus_successFalse_nullStatus() {
+		boolean success = AccountDAO.updateAccountStatus("Uchiha_Lover", null);
+		assertFalse(success);
+	}
+	
+	@Test
+	void updateAccountStatus_successFalse_invalidStatus() {
+		boolean success = AccountDAO.updateAccountStatus("caramel6", "s");
+		assertFalse(success);
+	}
+	
+	
 }
